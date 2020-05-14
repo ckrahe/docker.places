074400eb99bbeeb33b92d4eb99d0c072f1a72964
Chris Krahe <cpkrahe@evoforge.org>
2020-05-14 12:30:04 -0400

    Initial

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -x
+git clone https://github.com/ckrahe/mongo.places ~/mongo.places/
+git clone https://github.com/ckrahe/express.places ~/express.places/
+git clone https://github.com/ckrahe/nginx.places ~/nginx.places/
