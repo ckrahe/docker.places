a4c6bd0101ca2a090a31382db00fe3b082f9a7d2
Chris Krahe <chris@krahe.org>
2022-01-15 19:05:45 -0500

    Drop back to version 3.7

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-set -x
-git clone https://github.com/ckrahe/mongo.places ~/mongo.places/
-git clone https://github.com/ckrahe/express.places ~/express.places/
-git clone https://github.com/ckrahe/nginx.places ~/nginx.places/
+
+clone_if () {
+	if [ -d "$2" ]; then
+		echo "exists:" $2
+	else
+		echo "Cloning to " $2
+		git clone $1 $2
+	fi
+}
+
+clone_if git@github.com:ckrahe/mongo.places.git ../mongo.places/
+clone_if git@github.com:ckrahe/express.places.git ../express.places/
+clone_if git@github.com:ckrahe/nginx.places.git ../nginx.places/
